fbe459971e396062c67b739a06300d942e8ccfdf
JWahyu <71404267+JWahyu@users.noreply.github.com>
2020-09-30 21:01:33 +0200

    Jade / JWahyu
    Opdracht Renteoverzicht

--- src/Renteoverzicht.java
+++ src/Renteoverzicht.java
@@ -1,5 +1,69 @@
+import java.util.Scanner;
+
 public class Renteoverzicht {
     public static void main(String[] args) {
 
+        // Rente spaaroverzichtprogramma.
+
+        System.out.printf("%n―――――――――――――――――――――――――――――――%n");
+        System.out.printf("$$$ Welkom bij de Totaal-Niet-Corrupte bank! $$$$%n");
+        System.out.printf("―――――――――――――――――――――――――――――――%n");
+        System.out.printf("%nMinimale inleg bedraagt €100,-! Hieronder tref je onze rentetarieven:%n");
+
+        System.out.printf(
+                "€ 100,- t/m € 299,- → 3%% rente%n" +
+                "€ 300,- t/m € 599,- → 2%% rente%n" +
+                "€ 600,- t/m € 1000,- → 1%% rente%n" +
+                "Meer dan € 1000,- → 0,5%% rente%n");
+
+        // Invoer naam.
+
+        System.out.printf("%nAllereerst wil ik weten hoe je heet! Dus wat is je naam?%n");
+        var input = new Scanner(System.in);
+        var inputName = input.nextLine();
+
+        // Invoer inleg en/of uitvoer overzicht.
+
+        boolean check = true;
+        do {
+            System.out.printf("%nHoeveel wil je betalen %s?%n", inputName);
+            var investment = input.nextDouble();
+            var year = 1;
+            if (investment < 100) {
+                check = false;
+                System.out.printf("Slechts € %.2f? Niet zo gierig doen hé %s! Je moet wel minimaal € 100,- dokken!%n", investment, inputName);
+            } else if (investment >= 100 && investment < 300) {
+                check = true;
+                System.out.printf("%nAardig begin, %s! Voor je bedrag van € %.2f geniet je van 3%% rente. Hoe rijk ben je de komende 10 jaar? Zo rijk dus:%n", inputName, investment);
+                for (year = 1; year <= 10; year++) {
+                    System.out.format("Jaar %d: € %.2f %n", year, investment*=1.03);
+                }
+            } else if (investment >= 300 && investment < 599) {
+                check = true;
+                System.out.printf("%nDat is mooi, %s! Voor je bedrag van € %.2f geniet je van 2%% rente. Hoe rijk ben je de komende 10 jaar? Zo rijk dus:%n", inputName, investment);
+                for (year = 1; year <= 10; year++) {
+                    System.out.format("Jaar %d: € %.2f %n", year, investment*=1.02);
+                }
+            } else if (investment >= 600 && investment < 1000) {
+                check = true;
+                System.out.printf("%nDat is uitstekend, %s! Voor je bedrag van € %.2f geniet je van 1%% rente. Hoe rijk ben je de komende 10 jaar? Zo rijk dus:%n", inputName, investment);
+                for (year = 1; year <= 10; year++) {
+                    System.out.format("Jaar %d: € %.2f %n", year, investment*=1.01);
+                }
+            } else if (investment > 1000) {
+                check = true;
+                System.out.printf("%nWow kassa, %s! Voor je bedrag van € %.2f geniet je van 0,5%% rente. Hoe rijk ben je de komende 10 jaar? Zo rijk dus:%n", inputName, investment);
+                for (year = 1; year <= 10; year++) {
+                    System.out.format("Jaar %d: € %.2f %n", year, investment*=1.005);
+                }
+            }
+        } while (!check);
+
+        // Einde.
+        System.out.printf("%nDank je wel voor je geld %s! Maak maar daar geen zorgen om hoor... ;)%n", inputName);
+        System.out.printf("%n――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――%n");
+        System.out.printf("$$$ Tot de volgende keer bij de Totaal-Niet-Corrupte bank! Jouw sessie is hierbij beëindigd. $$$%n");
+        System.out.printf("――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――%n");
     }
 }
+
